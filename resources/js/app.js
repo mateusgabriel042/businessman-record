e3b5feb3a859cab00a9339b7425bdb80d33dba82
Mateus Gabriel <mateus.gabriel042@gmail.com>
2021-04-01 18:51:08 -0300

    Instalação, Configuração e teste do Vue.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+
+import App from './views/app.vue';
+
+const app = new Vue({
+	el: '#app',
+	components: {'app':App}
+});
---
+++ resources/js/views/app.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		{{message}}
+	</div>
+</template>
+
+<script type="text/babel">
+	const default_layout = "default";
+
+	export default {
+		computed: {},
+
+		data() {
+			return {
+				message: 'Hello World',
+			}
+		}
+	};
+</script>

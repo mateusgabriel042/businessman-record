0d8a6e9501b25f1f7190fcad88244a9575983313
Mateus Gabriel <mateus.gabriel042@gmail.com>
2021-04-14 23:13:59 -0300

    criando a rota de edição

--- resources/js/routers/router.js
+++ resources/js/routers/router.js
@@ -1,6 +1,7 @@
 import Register from '../views/businessmans/Register.vue';
 import List from '../views/businessmans/List.vue';
 import ViewRede from '../views/businessmans/ViewRede.vue';
+import Edit from '../views/businessmans/Edit.vue';
 
 const routes = [
 	{
@@ -12,6 +13,11 @@ const routes = [
 		path:'/listar',
 		name:'list',
 		component: List
+	},
+	{
+		path:'/edit/:id',
+		name:'edit',
+		component: Edit
 	},
 	{
 		path:'/rede/:id',

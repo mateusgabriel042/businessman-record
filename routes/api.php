78f862de7612d348ac48ca4c9de14091f44e800f
Mateus Gabriel <mateus.gabriel042@gmail.com>
2021-04-14 23:38:28 -0300

    adicionando rotas na API

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BusinessmansController;
 
-Route::resource('businessman', BusinessmansController::class)->only(['index','store','show','destroy']);
+Route::resource('businessman', BusinessmansController::class)->only(['index','store','show','destroy','edit','update']);
